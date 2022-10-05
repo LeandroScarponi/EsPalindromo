3c9a11dde7ad65d63537c5c4f8cbb87023d97592
Leandro.scarponi <scarponi.leandro@gmail.com>
2022-10-05 12:48:47 +0200

    Agregar archivos de proyecto.

---
+++ EsPalindromo/DAL/DalPalindromo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EsPalindromo.DAL
+{
+    class DalPalindromo
+    {
+        public static bool Palindromo(string dialogo)
+        {
+            int longitud = dialogo.Length;
+            int posicion;
+
+            if (longitud % 2 == 0)
+                posicion = longitud / 2 + 1;
+            else
+                posicion = (longitud + 1) / 2;
+
+            for (int i = 0; i < posicion; i++)
+            {
+                if (dialogo[i] != dialogo[longitud - 1 - i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ EsPalindromo/Program.cs
@@ -0,0 +1,21 @@
+﻿using EsPalindromo.DAL;
+using System;
+
+namespace EsPalindromo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ingrese una palabra para determinar si es palindromo o no:");
+            string palabra = Console.ReadLine();
+
+            if (DalPalindromo.Palindromo(palabra))
+                Console.WriteLine("es palindromo");
+            else
+                Console.WriteLine("no es palindromo");
+
+            Console.ReadKey();
+        }
+    }
+}
